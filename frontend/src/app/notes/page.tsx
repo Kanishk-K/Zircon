c6e78c0359bc442a7f1b61e8b888af5f23687ebd
Kanishk-K <kanishkkacholia@gmail.com>
2025-01-12 16:31:23 -0600

    🍡 Added sticky header and links

---
+++ frontend/src/app/notes/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>This is the notes page insert sample note here</div>
+  );
+}
--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -3,16 +3,20 @@ import { FaGithub } from "react-icons/fa6";
 
 export default function Navbar() {
     return (
-        <nav className="flex flex-row w-full justify-between p-6 max-w-[1376px] mb-8">
-            <Image className="h-10 w-auto" src="/vercel.svg" alt="Logo" width={173} height={150} />
-            <div className="hidden md:flex flex-row items-center gap-6">
-                <a href="#" className="hover:text-white">About</a>
-                <a href="#" className="hover:text-white">Notes</a>
-                <a href="#" className="hover:text-white">System Status</a>
-                <a href="#" className="hover:text-white flex flex-row items-center gap-2">Github <FaGithub /></a>
-                <a href="#" className="p-2 bg-brand text-background font-medium rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-md">Get Started</a>
+        <nav className="w-full flex flex-row justify-center sticky top-0 dark:bg-neutral-950 z-10 mb-8">
+            <div className="flex flex-row w-full justify-between p-6 max-w-[1376px]">
+                <a href="/">
+                    <Image className="h-10 w-auto" src="/vercel.svg" alt="Logo" width={173} height={150} />
+                </a>
+                <div className="hidden md:flex flex-row items-center gap-6">
+                    <a href="#" className="hover:text-white">About</a>
+                    <a href="/notes" className="hover:text-white">Notes</a>
+                    <a href="#" className="hover:text-white">System Status</a>
+                    <a href="#" className="hover:text-white flex flex-row items-center gap-2">Github <FaGithub /></a>
+                    <a href="#" className="p-2 bg-brand text-background font-medium rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-md">Get Started</a>
+                </div>
+                <a href="#" className="md:hidden text-brand font-medium rounded-lg flex items-center"><FaGithub size={'2em'} /></a>
             </div>
-            <a href="#" className="md:hidden text-brand font-medium rounded-lg flex items-center"><FaGithub size={'2em'} /></a>
         </nav>
     )
 }
