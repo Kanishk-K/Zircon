d06c397e19a76440610bdd4571ee149f1e292785
Kanishk-K <kanishkkacholia@gmail.com>
2025-01-22 11:43:57 -0600

    🎯 Remove dependency deadweight.

--- frontend/src/app/notes/[entryID]/error.tsx
+++ frontend/src/app/notes/[entryID]/error.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Error from "next/error"
 export default function ErrorPage(){
     return (
         <div className="flex w-full min-h-64 text-center text-5xl lg:text-6xl text-foreground">
