af986eab0dcc0bd04bc16d3d9f43c9834ad4bd4b
Kanishk-K <kanishkkacholia@gmail.com>
2025-02-16 09:17:14 -0600

    🔐 Auth endpoint operational

--- Backend/cmd/Auth/main.go
+++ Backend/cmd/Auth/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"encoding/json"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
+
+func GenerateStateOAuthCookie() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+
+	return state
+}
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	resp := events.APIGatewayProxyResponse{
@@ -16,13 +29,36 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		},
 		IsBase64Encoded: false,
 	}
-	retVal, err := json.Marshal(map[string]interface{}{"message": "Hello World"})
-	if err != nil {
-		resp.StatusCode = 500
-		return resp, err
+
+	/*
+		Buisness logic goes here
+	*/
+
+	// Get the PORT from environment variables
+	host := os.Getenv("HOST")
+	port := ""
+	if host == "" {
+		port = ":3000"
+		host = "http://localhost"
+		fmt.Println("No HOST environment variable detected, defaulting to", host)
+		fmt.Println("No PORT environment variable detected, defaulting to", port)
 	}
-	resp.StatusCode = 200
-	resp.Body = string(retVal)
+
+	GoogleOauthConfig := &oauth2.Config{
+		RedirectURL:  host + port + "/callback",
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
+
+	oauthState := GenerateStateOAuthCookie()
+	authURL := GoogleOauthConfig.AuthCodeURL(oauthState, oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+	resp.StatusCode = 302
+	resp.Headers["Location"] = authURL
 	return resp, nil
 }
 
