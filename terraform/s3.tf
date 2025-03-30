459d04016e123136565483131e1fb32760666663
Kanishk-K <kanishkkacholia@gmail.com>
2025-03-30 16:56:47 -0500

    🗃 Explicitly added static objects to s3.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -13,3 +13,24 @@ resource "aws_s3_bucket_policy" "read_content" {
   bucket = aws_s3_bucket.s3_bucket.bucket
   policy = data.aws_iam_policy_document.read_content.json
 }
+
+resource "aws_s3_object" "static_logo" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  key    = "background/logo.png"
+  source = "${path.module}/../backend/static/logo.png"
+  etag   = filemd5("${path.module}/../backend/static/logo.png")
+}
+
+resource "aws_s3_object" "static_minecraft" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  key    = "background/minecraft.mp4"
+  source = "${path.module}/../backend/static/minecraft.mp4"
+  etag   = filemd5("${path.module}/../backend/static/minecraft.mp4")
+}
+
+resource "aws_s3_object" "static_subway_surfers" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  key    = "background/subway_surfers.mp4"
+  source = "${path.module}/../backend/static/subway_surfers.mp4"
+  etag   = filemd5("${path.module}/../backend/static/subway_surfers.mp4")
+}
